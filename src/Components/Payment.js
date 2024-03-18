25638f5b94a151bebab0dc2d3b36eda6dfc726dc
HIS <hwanginsick@gmail.com>
2024-03-18 14:37:19 +0900

    branch update

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -26,14 +26,10 @@ const PaymentButton = styled.button`
 `;
 const Payment = (effect, deps) => {
   useEffect(() => {
-    const jquery = document.createElement("script");
-    jquery.src = "https://code.jquery.com/jquery-1.12.4.min.js";
     const iamport = document.createElement("script");
-    iamport.src = "https://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
-    document.head.appendChild(jquery);
+    iamport.src = "https://cdn.iamport.kr/v1/iamport.js";
     document.head.appendChild(iamport);
     return () => {
-      document.head.removeChild(jquery);
       document.head.removeChild(iamport);
     };
   }, []);
@@ -43,34 +39,25 @@ const Payment = (effect, deps) => {
     IMP.init("imp83485354");
     // 결제 데이터 정의
     const data = {
-      pg: "kcp.AO09C", // PG사 코드.PG상점아이디
-      pay_method: "card", // 결제수단
+      pg: "kcp.AO09C", // PG사 코드.PG상점아이디 //고정값
+      pay_method: "card", // 결제수단 //고정값
       merchant_uid: `mid_${new Date().getTime()}`, // 주문번호
       amount: 1000, // 결제금액
-      name: "아임포트 결제 데이터 분석", // 주문명
+      name: "테스트결제", // 주문명
       buyer_name: "홍길동", // 구매자 이름
       buyer_tel: "01012341234", // 구매자 전화번호
-      buyer_email: "example@example", // 구매자 이메일
+      buyer_email: "example@example.com", // 구매자 이메일
       buyer_addr: "신사동 661-16", // 구매자 주소
-      buyer_postcode: "06018", // 구매자 우편번호
     };
     IMP.request_pay(data, callback);
   };
 
   const callback = (response) => {
-    const {
-      success,
-      error_msg,
-      imp_uid,
-      merchant_uid,
-      pay_method,
-      paid_amount,
-      status,
-    } = response;
-    if (success) {
+    if (response.success) {
       alert("결제 성공");
+      console.log(response);
     } else {
-      alert(`결제 실패 : ${error_msg}`);
+      alert(`결제 실패 : ${response.error_msg}`);
     }
   };
 
