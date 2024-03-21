f8bc7fd235b9d847463a9b9e3872731d90b1f588
HIS <hwanginsick@gmail.com>
2024-03-21 13:14:38 +0900

    시간 선택에 따른 예매 좌석 구현

--- src/Pages/Ticketing.js
+++ src/Pages/Ticketing.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Payment from "../Components/Payment";
 import { useNavigate } from "react-router";
-import jsonData from "../dummy/show_detail.json";
+import jsonDataDetail from "../dummy/show_detail.json";
+import jsonDataTime from "../dummy/show_time.json";
 import Calendar from "../Components/Calendar";
 
-const data = jsonData;
+const dataDetail = jsonDataDetail;
+const dataTime = jsonDataTime;
 
+const TicketingWrapper = styled.div`
+  margin: 0 auto;
+  width: 1120px;
+`;
 const ContentWrpper = styled.div`
   margin: 30px 10px;
   text-align: center;
@@ -30,10 +36,10 @@ const ContentTitle = styled.div`
 `;
 
 const NavLocation = styled.div`
-  font-size: 14px;
   color: #666666;
   left: 0;
   span {
+    font-size: 14px;
     margin-right: 8px;
     cursor: pointer;
   }
@@ -48,7 +54,7 @@ const ContentDetail = styled.div`
 const ContentDetailPoster = styled.div`
   min-width: 400px;
   height: 573px;
-  background-image: url(${data.poster});
+  background-image: url(${dataDetail.poster});
   background-size: cover;
   background-position: center;
 `;
@@ -72,13 +78,14 @@ const ContentDetailInfo = styled.div`
   }
 
   span {
+    text-align: left;
     margin-left: 15px;
   }
 `;
 
 const SeatWrapper = styled.div`
   display: grid;
-  grid-template-columns: 2fr 2fr 1fr;
+  grid-template-columns: 2fr 2fr 1.8fr;
   height: 350px;
   margin: 30px 10px;
   text-align: center;
@@ -91,18 +98,62 @@ const SeatBox = styled.div`
   padding: 20px;
   border-left: 1px solid #bfbfbf;
   border-radius: 0 15px 15px 0;
+  align-items: flex-start;
+  justify-content: space-between;
 `;
 
 const BoxHeader = styled.div`
   display: block;
-  width: 150px;
+  width: 100px;
   text-align: left;
   span {
     display: block;
-    font-size: 20px;
+    font-size: 18px;
     margin-top: 0;
     font-weight: 700;
   }
+`;
+
+const TimeItemList = styled.div`
+  overflow-y: overlay;
+  scrollbar-width: thin;
+  scrollbar-color: rgba(36, 36, 40, 0.4) rgba(0, 0, 0, 0);
+  padding-top: 24px;
+  width: 257px;
+  height: 100%;
+  border: 0;
+  vertical-align: top;
+`;
+
+const TimeItemBtn = styled.button`
+  padding: 11px 18px 11px 15px;
+  width: 100%;
+  border: 1px solid #373a42;
+  border-radius: 12px;
+  text-align: left;
+  margin-bottom: 10px;
+  height: 60px;
+  font-size: 18px;
+  background-color: #fff;
+
+  &.select {
+    color: #fc1055;
+    border-color: #fc1055;
+  }
+`;
+
+const SeatList = styled.ul`
+  padding: 0;
+`;
+const SeatItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+const SeatTimeData = styled.span`
+  color: #ab003c;
+  font-weight: 700;
 `;
 
 const Ticketing = () => {
@@ -114,10 +165,47 @@ const Ticketing = () => {
     navigate("/showlist");
   };
   const clickGenre = () => {
-    navigate(`/showlist/${data.genrenm}`);
+    navigate(`/showlist/${dataDetail.genrenm}`);
   };
+  const [selectDate, setSelectDate] = useState("");
+  const [showTimes, setShowTimes] = useState([]);
+  const [showSeats, setShowSeats] = useState([]);
+  const [selectData, setSelectData] = useState([]);
+  const [selectedTimeData, setSelectedTimeData] = useState(null);
+
+  function convertToTimeFormat(arr) {
+    return arr.map((time) => {
+      const hour = time.slice(0, 2);
+      const minute = time.slice(2);
+      return `${hour}시 ${minute}분`;
+    });
+  }
+  const handleDataChange = (newData) => {
+    setSelectDate(newData);
+    const resultTime = dataTime.filter((data) => data.playDate === newData);
+
+    let resultShowTime = resultTime.map((row) => row.playTime);
+    setShowTimes(convertToTimeFormat(resultShowTime));
+
+    setShowSeats(resultTime);
+    setSelectData(resultShowTime);
+  };
+
+  const [select, setSelect] = useState("");
+  const handleClick = (time) => {
+    setSelect(time);
+
+    let str = time;
+    let replaced_str = str.replace(/시|분|\s/g, "");
+
+    const selectedData = showSeats.find(
+      (data) => data.playTime === replaced_str
+    );
+    setSelectedTimeData(selectedData);
+  };
+
   return (
-    <>
+    <TicketingWrapper>
       <ContentWrpper>
         <ContentTitle>
           <NavLocation>
@@ -125,12 +213,12 @@ const Ticketing = () => {
             <span>/</span>
             <span onClick={clickShowList}>공연전시예매</span>
             <span>/</span>
-            <span onClick={clickGenre}>{data.genrenm}</span>
+            <span onClick={clickGenre}>{dataDetail.genrenm}</span>
           </NavLocation>
-          <strong>{data.prfnm}</strong>
+          <strong>{dataDetail.prfnm}</strong>
           <br />
           <span>
-            {data.prfpdfrom} ~ {data.prfpdto}
+            {dataDetail.prfpdfrom} ~ {dataDetail.prfpdto}
           </span>
         </ContentTitle>
         <ContentDetail>
@@ -139,33 +227,33 @@ const Ticketing = () => {
             <ul>
               <li>공연일시</li>
               <span>
-                {data.prfpdfrom} ~ {data.prfpdto}
+                {dataDetail.prfpdfrom} ~ {dataDetail.prfpdto}
               </span>
             </ul>
             <ul>
               <li>시간</li>
-              <span>{data.dtguidance}</span>
+              <span>{dataDetail.dtguidance}</span>
             </ul>
             <ul>
               <li>장소</li>
-              <span>{data.fcltynm}</span>
+              <span>{dataDetail.fcltynm}</span>
             </ul>
             <ul>
               <li>관람시간</li>
-              <span>{data.prfruntime}</span>
+              <span>{dataDetail.prfruntime}</span>
             </ul>
             <ul>
               <li>관람등급</li>
-              <span>{data.prfage}</span>
+              <span>{dataDetail.prfage}</span>
             </ul>
             <ul>
               <li>문의</li>
-              <span>📞{data.telno}</span>
+              <span>📞{dataDetail.telno}</span>
             </ul>
             <hr />
             <ul>
               <li style={{ color: "#AB003C" }}>티켓가격</li>
-              <span>{data.pcseguidance}</span>
+              <span>{dataDetail.pcseguidance}</span>
             </ul>
           </ContentDetailInfo>
         </ContentDetail>
@@ -176,22 +264,67 @@ const Ticketing = () => {
             <span style={{ color: "#AB003C" }}>01</span>
             <span>날짜 선택</span>
           </BoxHeader>
-          <Calendar></Calendar>
+          <Calendar onDataChange={handleDataChange}></Calendar>
         </SeatBox>
         <SeatBox>
           <BoxHeader>
             <span style={{ color: "#AB003C" }}>02</span>
             <span>시간 선택</span>
           </BoxHeader>
+          <TimeItemList>
+            {showTimes.map((time, index) => (
+              <TimeItemBtn
+                key={index}
+                onClick={() => handleClick(time)}
+                className={`${select === time ? "select" : ""}`}
+              >
+                {time}
+              </TimeItemBtn>
+            ))}
+          </TimeItemList>
         </SeatBox>
-        <SeatBox style={{ backgroundColor: "#f6f6f6" }}>
-          <BoxHeader>
+        <SeatBox style={{ backgroundColor: "#f6f6f6", display: "block" }}>
+          <BoxHeader style={{ width: "120px" }}>
             <span style={{ color: "#AB003C" }}>예매 가능 좌석</span>
           </BoxHeader>
+          <>
+            {selectedTimeData && (
+              <SeatList>
+                <SeatItem>
+                  <span>VIP석</span>
+                  <span>
+                    <SeatTimeData>{selectedTimeData.VIP}</SeatTimeData>
+                    <span>석</span>
+                  </span>
+                </SeatItem>
+                <SeatItem>
+                  <span>R석</span>
+                  <span>
+                    <SeatTimeData>{selectedTimeData.R}</SeatTimeData>
+                    <span>석</span>
+                  </span>
+                </SeatItem>
+                <SeatItem>
+                  <span>S석</span>
+                  <span>
+                    <SeatTimeData>{selectedTimeData.S}</SeatTimeData>
+                    <span>석</span>
+                  </span>
+                </SeatItem>
+                <SeatItem>
+                  <span>A석</span>
+                  <span>
+                    <SeatTimeData>{selectedTimeData.A}</SeatTimeData>
+                    <span>석</span>
+                  </span>
+                </SeatItem>
+              </SeatList>
+            )}
+          </>
         </SeatBox>
       </SeatWrapper>
       <Payment></Payment>
-    </>
+    </TicketingWrapper>
   );
 };
 
